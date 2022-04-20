f8b724f5ef5c75da10a0a6dcc14b1660fcf18031
Soner <s.sayakci@shopware.com>
2022-04-20 19:23:06 +0200

    improve default traefik ssl settings

--- traefik.nix
+++ traefik.nix
@@ -38,7 +38,17 @@
     http.middlewares = {
       web-redirect.redirectScheme.scheme = "https";
     };
-    http.services = {};
+    tls.options.default = {
+      minVersion = "VersionTLS12";
+      cipherSuites = [
+        "TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256"
+        "TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256"
+        "TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384"
+        "TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384"
+        "TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305"
+        "TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305"
+      ];
+    };
   };
 
   systemd.services.traefik.serviceConfig.EnvironmentFile = "/srv/traefik/.env";
