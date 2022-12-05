bfacb68f5df6ecc9dd030944b2b5df5eb478921d
Shyim <github@shyim.de>
2022-12-05 23:55:33 +0100

    screego support arm64

--- pkgs/screego/default.nix
+++ pkgs/screego/default.nix
@@ -1,14 +1,22 @@
 { stdenv, lib, fetchurl, makeWrapper }:
 
-stdenv.mkDerivation rec {
-  name = "screego";
+let
   version = "1.7.4";
-
-  src = fetchurl {
-    url =
-      "https://github.com/screego/server/releases/download/v1.7.4/screego_1.7.4_linux_amd64.tar.gz";
-    sha256 = "1y2kjg8pkfvj28d9yh5bzf55r324p36ssydp44zzw5gda5zrmabc";
+  soruces = {
+    "x86_64-linux" = fetchurl {
+      url = "https://github.com/screego/server/releases/download/v1.7.4/screego_1.7.4_linux_amd64.tar.gz";
+      sha256 = "1y2kjg8pkfvj28d9yh5bzf55r324p36ssydp44zzw5gda5zrmabc";
+    };
+    "aarch64-linux" = fetchurl {
+      url = "https://github.com/screego/server/releases/download/v1.7.4/screego_1.7.4_linux_arm64.tar.gz";
+      sha256 = "1dip76y9vcww7b4ypjqhn2i59pkgzpg226jg7k07jk6wl74d0axa";
+    };
   };
+in stdenv.mkDerivation rec {
+  name = "screego";
+  inherit version;
+
+  src = sources.${stdenv.hostPlatform.system} or (throw "Unsupported platform for screego: ${stdenv.hostPlatform.system}");
 
   nativeBuildInputs = [ makeWrapper ];
   dontBuild = true;
