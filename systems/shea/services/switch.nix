279f3508ba53cb47120a14b4468b3cb9b65424e0
Soner Sayakci <s.sayakci@shopware.com>
2022-12-06 13:00:28 +0000

    complete switch migration

--- systems/shea/services/switch.nix
+++ systems/shea/services/switch.nix
@@ -3,8 +3,28 @@
 {
   services.caddy.virtualHosts."http://nut.shyim.de" = {
     extraConfig = ''
-      reverse_proxy :9000
+      reverse_proxy unix/var/run/nut/web.sock
       encode gzip
     '';
+  };
+
+  users.users.nut = {
+    description = "The nut service user";
+    group = "caddy";
+    isSystemUser = true;
+  };
+
+  systemd.services.nut = {
+    wantedBy = [ "multi-user.target" ];
+    serviceConfig = {
+      ExecStart = "/opt/nut server --socket /var/run/nut/web.sock";
+      User = "nut";
+      Group = "caddy";
+      StateDirectory = "nut";
+      RuntimeDirectory = "nut";
+      StateDirectoryMode = "0770";
+      RuntimeDirectoryMode = "0770";
+      UMask = "0002";
+    };
   };
 }
