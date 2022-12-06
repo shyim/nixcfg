cc000c1141f760b63bf057b95d2e884ed3831e4b
Soner Sayakci <s.sayakci@shopware.com>
2022-12-06 00:45:30 +0000

    enable mysql

--- systems/shea/services.nix
+++ systems/shea/services.nix
@@ -3,9 +3,22 @@
 {
   services.openssh.enable = true;
 
-  virtualisation.docker.enable = true;
-
-  virtualisation.docker.daemon.settings = {
-    live-restore = true;
+  # The Lounge
+  services.thelounge.enable = true;
+  services.thelounge.extraConfig = {
+    reverseProxy = true;
   };
+
+  # MySQL
+  services.mysql.enable = true;
+  services.mysql.package = pkgs.mysql80;
+  services.mysql.initialDatabases = [ { name = "wakapi"; } ];
+  services.mysql.ensureUsers = [
+    {
+      name = "wakapi";
+      ensurePermissions = {
+        "wakapi.*" = "ALL PRIVILEGES";
+      };
+    }
+  ];
 }
