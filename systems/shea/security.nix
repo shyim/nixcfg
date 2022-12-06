ed0536f141e9bbd87d2c64e947ee9e2bd5c2df44
Shyim <s.sayakci@shopware.com>
2022-12-06 15:39:16 +0100

    Update security.nix

--- systems/shea/security.nix
+++ systems/shea/security.nix
@@ -3,6 +3,7 @@
 {
   networking.firewall.allowedTCPPorts = [ 22 80 443 ];
   networking.firewall.allowedUDPPorts = [ 443 ];
+  networking.firewall.enable = false;
 
   users.users.root.openssh.authorizedKeys.keys = [
     # GitHub Action
