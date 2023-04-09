8148cee840b5fd842b9708031ff460079b290fe3
Soner Sayakci <s.sayakci@shopware.com>
2023-04-09 20:21:15 +0200

    feat: update wakapi to 2.7.0

--- pkgs/wakapi/default.nix
+++ pkgs/wakapi/default.nix
@@ -5,20 +5,20 @@
 }:
 buildGoModule rec {
   pname = "wakapi";
-  version = "2.6.2";
+  version = "2.7.0";
 
   src = fetchFromGitHub {
     owner = "muety";
     repo = "wakapi";
     rev = version;
-    sha256 = "sha256-yMxcePwBUteqrdfvDjZSRInOXMFmwaFoVBihcMQFTME=";
+    sha256 = "sha256-1EMSrHx6Tx58voz5veyNZg1gnubuGyg2K4dg2QdzmMw=";
   };
 
   excludedPackages = [ "scripts" ];
 
   CGO_CFLAGS = lib.optionals stdenv.cc.isGNU [ "-Wno-return-local-addr" ];
 
-  vendorSha256 = "sha256-sfx8qlmJrS0hkD6DSvKqfnBDbxj8eNA3hnprSwA2fSI=";
+  vendorSha256 = "sha256-0wHXULDKyXYBTGxfSQXT/5NidPtSnx7ujb8vyczmE38=";
 
   meta = with lib; {
     homepage = "https://wakapi.dev";
