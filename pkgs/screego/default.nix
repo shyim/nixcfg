fadce304e7282fa2b9bc24868d239e0638229fb3
Soner Sayakci <s.sayakci@shopware.com>
2023-04-13 00:07:16 +0200

    feat: force screego to use turn server

--- pkgs/screego/default.nix
+++ pkgs/screego/default.nix
@@ -1,32 +1,67 @@
 { stdenv
 , lib
+, fetchFromGitHub
+, buildGoModule
+, fetchYarnDeps
+, fixup_yarn_lock
+, nodejs
 , fetchurl
-, makeWrapper
+, yarn
 ,
 }:
-let
-  version = "1.8.1";
-  sources = {
-    "aarch64-linux" = fetchurl {
-      url = "https://github.com/screego/server/releases/download/v${version}/screego_${version}_linux_arm64.tar.gz";
-      sha256 = "sha256-joXVSNpkShN4FPAa5Voj5i0cjdkNyVBYO36anXMvXok=";
-    };
-  };
-in
-stdenv.mkDerivation rec {
+buildGoModule rec {
   name = "screego";
-  inherit version;
+  version = "1.8.1";
 
-  src = sources.${stdenv.hostPlatform.system} or (throw "Unsupported platform for screego: ${stdenv.hostPlatform.system}");
+  src = fetchFromGitHub {
+    owner = "screego";
+    repo = "server";
+    rev = "v${version}";
+    hash = "sha256-n17LFEzaqWQL2A4VUmiZFbuece2yl0/GirvXLvCr++w=";
+  };
 
-  nativeBuildInputs = [ makeWrapper ];
-  dontBuild = true;
-  setSourceRoot = "sourceRoot=`pwd`";
+  patches = [
+    ./force-turn.patch
+  ];
 
-  installPhase = ''
-    mkdir -p $out/bin
-    cp screego $out/bin/
+  CGO_ENABLED = 0;
+  tags = [ "netgo" "osusergo" ];
+  ldflags = [
+    "-s"
+    "-w"
+    "-X main.version=${version}"
+    "-X main.commitHash=00"
+    "-X main.mode=prod"
+  ];
+
+  vendorHash = "sha256-KWAeoL3qvTxenhs0XtEHY1mpPzSiQMniqPnRLELwLKg=";
+
+  offlineCache = fetchYarnDeps {
+    yarnLock = src + "/ui/yarn.lock";
+    hash = "sha256-uUIHuiZeyjkOoYLiHa4KlkCZ5OPxFajiA72arueWmKY=";
+  };
+
+  preBuild = ''
+    export HOME=$TEMPDIR
+    cd ui
+
+    yarn config --offline set yarn-offline-mirror ${offlineCache}
+    fixup_yarn_lock yarn.lock
+    yarn install --offline
+    NODE_ENV=production node node_modules/.bin/react-scripts --max-old-space-size=3000 build
+
+    cd ..
   '';
+
+  postInstall = ''
+    mv $out/bin/server $out/bin/screego
+  '';
+
+  nativeBuildInputs = [
+    fixup_yarn_lock
+    nodejs
+    yarn
+  ];
 
   meta = {
     homepage = "https://github.com/screego/server";
--- systems/shea/services/screego.nix
+++ systems/shea/services/screego.nix
@@ -3,16 +3,43 @@
 , myFlake
 , ...
 }: {
+  sops.secrets.coturn_secret = {
+    owner = "turnserver";
+    restartUnits = [ "coturn.service" ];
+  };
+
+  sops.secrets.screego_env = {
+    restartUnits = [ "screego.service" ];
+  };
+
   users.users.screego = {
     description = "The screego service user";
     group = "caddy";
     isSystemUser = true;
   };
 
+  services.coturn = {
+    enable = true;
+    use-auth-secret = true;
+    static-auth-secret-file = config.sops.secrets.coturn_secret.path;
+    realm = "shea.shyim.de";
+    extraConfig = ''
+    external-ip=[2603:c020:800e:2300:af23:304d:7d5c:bed8]
+    external-ip=138.2.153.89
+    '';
+    listening-ips = [
+      "10.0.0.106"
+      "2603:c020:800e:2300:af23:304d:7d5c:bed8"
+    ];
+    min-port = 62000;
+    max-port = 64000;
+  };
+
   systemd.services.screego = {
     wantedBy = [ "multi-user.target" ];
     environment = {
-      SCREEGO_EXTERNAL_IP = "138.2.153.89,2603:c020:800e:2300:af23:304d:7d5c:bed8";
+      SCREEGO_TURN_EXTERNAL_IP = "2603:c020:800e:2300:af23:304d:7d5c:bed8,138.2.153.89";
+      SCREEGO_TURN_EXTERNAL_PORT = "3478";
       SCREEGO_TRUST_PROXY_HEADERS = "true";
       SCREEGO_AUTH_MODE = "none";
       SCREEGO_SERVER_ADDRESS = "127.0.0.1:3412";
@@ -20,6 +47,7 @@
     };
     serviceConfig = {
       ExecStart = "${myFlake.packages."aarch64-linux".screego}/bin/screego serve";
+      EnvironmentFile = config.sops.secrets.screego_env.path;
       User = "screego";
       Group = "caddy";
       RuntimeDirectory = "screego";
