82d83ec7ebb394099acb806937d74e4ee545cca8
Soner Sayakci <s.sayakci@shopware.com>
2023-04-14 11:29:09 +0200

    feat: improve docker start script

--- home/packages.nix
+++ home/packages.nix
@@ -9,17 +9,18 @@
 
 let
   dockerUp = pkgs.writeShellScriptBin "docker-up" ''
-    hcloud server create --image docker-ce --location hel1 --type cpx31 --name=docker-local --ssh-key 7588395
+    set -e
+    hcloud server create --image docker-ce --location fsn1 --type cpx31 --name=docker-local --ssh-key 7588395
     docker context rm -f hetzner
     SERVER_IP=$(hcloud server describe docker-local -o json | jq .public_net.ipv4.ip -r)
     docker context create hetzner --docker "host=ssh://root@$SERVER_IP"
     docker context use hetzner
-    ssh-keyscan -R "$SERVER_IP"
+    ssh-keygen -R "$SERVER_IP"
     ssh-keyscan "$SERVER_IP" >> ~/.ssh/known_hosts
   '';
   dockerDown = pkgs.writeShellScriptBin "docker-down" ''
-    hcloud server delete docker-local
     docker context rm -f hetzner
+    hcloud server delete docker-local
   '';
 in {
   home.packages = with pkgs; [
