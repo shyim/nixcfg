14e1f5b196679dbbb486262c72d596dce6a300ac
Soner Sayakci <s.sayakci@shopware.com>
2023-04-20 17:02:14 +0200

    feat: change public key

--- systems/shea/security.nix
+++ systems/shea/security.nix
@@ -7,7 +7,7 @@
 
   users.users.root.openssh.authorizedKeys.keys = [
     # Nix Builder
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIE+0y1Z/taJj1sQPYErWmXx2dt7Ol/b8zLVNcCvNw9a"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILuSBzDcfpg0wc/E9KOPVa1oSvwDYISnbZSTxHUodvKv"
 
     # GitHub Action
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEkbGqwlPvIaMBd5UV1disj1GtPm0eQsvx9mOdRBbNis"
