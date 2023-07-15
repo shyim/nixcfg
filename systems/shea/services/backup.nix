a4055c87f280f8400826f9636d44b914f85b089f
Soner Sayakci <s.sayakci@shopware.com>
2023-07-15 11:25:35 +0200

    feat: enable backups again

--- systems/shea/services/backup.nix
+++ systems/shea/services/backup.nix
@@ -2,36 +2,36 @@
 , pkgs
 , ...
 }: {
-  # sops.secrets.rclone_config = {
-  #   restartUnits = [ "rclone-restic.service" ];
-  # };
-  # sops.secrets.restic_env = {
-  #   restartUnits = [ "rclone-restic.service" ];
-  # };
+  sops.secrets.rclone_config = {
+    restartUnits = [ "rclone-restic.service" ];
+  };
+  sops.secrets.restic_env = {
+    restartUnits = [ "rclone-restic.service" ];
+  };
 
-  # systemd.services.rclone-restic = {
-  #   requires = [ "network-online.target" ];
-  #   partOf = [ "restic.service" ];
-  #   serviceConfig.ExecStart = "${pkgs.rclone}/bin/rclone serve restic Backup:backup/shea --config ${config.sops.secrets.rclone_config.path} --addr 127.0.0.1:7465";
-  #   serviceConfig.Type = "notify";
-  # };
+  systemd.services.rclone-restic = {
+    requires = [ "network-online.target" ];
+    partOf = [ "restic.service" ];
+    serviceConfig.ExecStart = "${pkgs.rclone}/bin/rclone serve restic Dropbox:shyim/backup/shea --config ${config.sops.secrets.rclone_config.path} --addr 127.0.0.1:7465";
+    serviceConfig.Type = "notify";
+  };
 
-  # systemd.services.restic = {
-  #   requires = [ "rclone-restic.service" ];
-  #   environment = {
-  #     RESTIC_REPOSITORY = "rest:http://localhost:7465/";
-  #   };
-  #   serviceConfig.EnvironmentFile = config.sops.secrets.restic_env.path;
-  #   serviceConfig.Type = "oneshot";
-  #   serviceConfig.ExecStart = "${pkgs.restic}/bin/restic backup /var/lib/mysql /var/lib/thelounge";
-  # };
+  systemd.services.restic = {
+    requires = [ "rclone-restic.service" ];
+    environment = {
+      RESTIC_REPOSITORY = "rest:http://localhost:7465/";
+    };
+    serviceConfig.EnvironmentFile = config.sops.secrets.restic_env.path;
+    serviceConfig.Type = "oneshot";
+    serviceConfig.ExecStart = "${pkgs.restic}/bin/restic backup /var/lib/mysql /var/lib/thelounge /var/lib/paperless";
+  };
 
-  # systemd.timers.restic = {
-  #   wantedBy = [ "timers.target" ];
-  #   partOf = [ "restic.service" ];
-  #   timerConfig = {
-  #     OnCalendar = [ "daily" ];
-  #     Unit = "restic.service";
-  #   };
-  # };
+  systemd.timers.restic = {
+    wantedBy = [ "timers.target" ];
+    partOf = [ "restic.service" ];
+    timerConfig = {
+      OnCalendar = [ "daily" ];
+      Unit = "restic.service";
+    };
+  };
 }
