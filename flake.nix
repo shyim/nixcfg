3ab29d85008772e0d119e2ab7407130abd3a362a
Soner Sayakci <s.sayakci@shopware.com>
2023-10-02 17:08:10 +0200

    chore: update flakes

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
 
     # Darwin Inputs
     darwin.url = "github:lnl7/nix-darwin";
