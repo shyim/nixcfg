382cecbade7e1846b45d1962cc3d490e3def5bbe
Soner Sayakci <s.sayakci@shopware.com>
2023-10-17 21:39:18 +0200

    fix: cleanup server

--- systems/shea/services/default.nix
+++ systems/shea/services/default.nix
@@ -3,13 +3,9 @@
 , ...
 }: {
   imports = [
-    ./backup.nix
-    ./rclone.nix
     ./screego.nix
     ./switch.nix
-    ./tailscale.nix
     ./thelounge.nix
     ./wakapi.nix
-    ./matrix.nix
   ];
 }
