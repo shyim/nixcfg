17d752160e82d2032fa1f8bd278291f0b81bd132
Soner Sayakci <s.sayakci@gmail.com>
2024-05-28 10:20:17 +0200

    update lock

--- home/programs/shell/default.nix
+++ home/programs/shell/default.nix
@@ -103,7 +103,7 @@ in
   programs.atuin = {
     enable = true;
     enableFishIntegration = true;
-    flags = [  "--disable-up-arrow" ];
+    flags = [ "--disable-up-arrow" ];
     settings = {
       auto_sync = true;
       sync_frequency = "5m";
