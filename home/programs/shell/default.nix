7a04b33b88169115b75baf4ffc9b7e220743c5b1
Soner Sayakci <s.sayakci@gmail.com>
2024-07-30 11:26:15 +0200

    disable buildkit tty

--- home/programs/shell/default.nix
+++ home/programs/shell/default.nix
@@ -69,6 +69,7 @@ in
     '';
     environmentVariables = {
       OBJC_DISABLE_INITIALIZE_FORK_SAFETY = "YES";
+      BUILDKIT_PROGRESS = "plain";
     };
   };
 
@@ -76,6 +77,7 @@ in
     enable = true;
     loginShellInit = ''
       export OBJC_DISABLE_INITIALIZE_FORK_SAFETY=YES
+      export BUILDKIT_PROGRESS=plain
       set fish_greeting
 
       function kn-switch
