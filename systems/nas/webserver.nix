7f3d0548e25c2a35e65e443e9f890570d07b7ee0
Soner Sayakci <s.sayakci@gmail.com>
2025-05-12 10:45:06 +0200

    fix: correct virtualHost URL format and remove ACME config for nut subdomain

--- systems/nas/webserver.nix
+++ systems/nas/webserver.nix
@@ -49,8 +49,7 @@
       plugins = [ "github.com/tailscale/caddy-tailscale@v0.0.0-20250207163903-69a970c84556" ];
       hash = "sha256-USKNTAvxmuxzhqA8e8XERr1U8513ONG54Md5vcDUERg=";
     };
-    virtualHosts."nut.shyim.de" = {
-      useACMEHost = "shyim.de";
+    virtualHosts."http://nut.shyim.de" = {
       extraConfig = ''
         reverse_proxy localhost:9000
       '';
