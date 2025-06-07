ad02882d98e8e44d77ff572cfae3dffd9fe7c934
Soner Sayakci <s.sayakci@gmail.com>
2025-06-07 15:40:34 +0200

    feat: add demo virtual host configuration for nginx with ACME and SSL support

--- systems/shea/services/shopmon.nix
+++ systems/shea/services/shopmon.nix
@@ -18,4 +18,12 @@
     };
   };
 
+  services.nginx.virtualHosts."demo.fos.gg" = {
+    enableACME = true;
+    forceSSL = true;
+    locations."/" = {
+      proxyPass = "http://127.0.0.1:3070";
+      proxyWebsockets = true;
+    };
+  };
 }
