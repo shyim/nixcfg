2955b2db78763a2920ba1f483c52b78dcd251a29
Soner Sayakci <s.sayakci@gmail.com>
2025-08-15 18:27:25 +0200

    feat: remove pangolin again

--- systems/shea/services/traefik.nix
+++ systems/shea/services/traefik.nix
@@ -1,94 +1,51 @@
-{ config, ... }:
-{
-  sops.secrets.traefik = { };
-
-  services.traefik = {
-    enable = true;
-
-    staticConfigOptions = {
-      entryPoints = {
-        web = {
-          address = ":80";
-          asDefault = true;
-          http.redirections.entrypoint = {
-            to = "websecure";
-            scheme = "https";
-          };
-        };
-
-        websecure = {
-          address = ":443";
-          asDefault = true;
-          http.tls.certResolver = "letsencrypt";
-        };
-      };
-
-      log = {
-        level = "INFO";
-        filePath = "${config.services.traefik.dataDir}/traefik.log";
-        format = "json";
-      };
-
-      certificatesResolvers.letsencrypt.acme = {
-        email = "postmaster@YOUR.DOMAIN";
-        storage = "${config.services.traefik.dataDir}/acme.json";
-        httpChallenge.entryPoint = "web";
-      };
-
-      api.dashboard = true;
-      # Access the Traefik dashboard on <Traefik IP>:8080 of your server
-      # api.insecure = true;
-    };
-
-    dynamicConfigOptions = {
-      http.routers = {};
-      http.services = {};
-    };
-  };
+{ config, ... }: {
+  sops.secrets.traefik = {};
 
   services.docker-compose.traefik.config = {
-    services = {
-      traefik = {
-        image = "traefik";
-        volumes = [
-          "/var/run/docker.sock:/var/run/docker.sock"
-          "traefik_data:/data"
+    services.traefik = {
+      image = "traefik";
+      volumes = [
+        "/var/run/docker.sock:/var/run/docker.sock"
+        "traefik_data:/data"
+      ];
+      env_file = config.sops.secrets.traefik.path;
 
-          "/var/lib/pangolin/traefik:/etc/traefik:ro"
-        ];
-        env_file = config.sops.secrets.traefik.path;
+      command = [
+        "--entrypoints.web.address=:80"
+        "--entrypoints.websecure.address=:443"
 
-        depends_on.pangolin.condition = "service_healthy";
-        network_mode = "service:gerbil";
+        # Metrics
+        "--metrics.prometheus=true"
+        "--metrics.prometheus.addrouterslabels=true"
 
-        command = [
-          "--configFile=/etc/traefik/traefik_config.yml"
+        # Enable Docker provider
+        "--providers.docker=true"
+        "--providers.docker.exposedbydefault=false"
 
-          "--entrypoints.web.address=:80"
-          "--entrypoints.websecure.address=:443"
+        # DNS Challenge for Cloudflare
+        "--certificatesresolvers.cloudflare.acme.tlschallenge=false"
+        "--certificatesresolvers.cloudflare.acme.dnschallenge=true"
+        "--certificatesresolvers.cloudflare.acme.dnschallenge.provider=cloudflare"
+        "--certificatesresolvers.cloudflare.acme.dnschallenge.resolvers=1.1.1.1:53,8.8.8.8:53"
+        "--certificatesresolvers.cloudflare.acme.email=acme@shyim.de"
+        "--certificatesresolvers.cloudflare.acme.storage=/data/acme.json"
 
-          # Metrics
-          "--metrics.prometheus=true"
-          "--metrics.prometheus.addrouterslabels=true"
+        # Global Redirect
+        "--entrypoints.web.http.redirections.entrypoint.to=websecure"
+        "--entrypoints.web.http.redirections.entrypoint.scheme=https"
+        "--entrypoints.web.http.redirections.entrypoint.permanent=true"
 
-          # Enable Docker provider
-          "--providers.docker=true"
-          "--providers.docker.exposedbydefault=false"
+        "--log.level=INFO"
+        "--accesslog=true"
+      ];
 
-          # DNS Challenge for Cloudflare
-          "--certificatesresolvers.cloudflare.acme.tlschallenge=false"
-          "--certificatesresolvers.cloudflare.acme.dnschallenge=true"
-          "--certificatesresolvers.cloudflare.acme.dnschallenge.provider=cloudflare"
-          "--certificatesresolvers.cloudflare.acme.dnschallenge.resolvers=1.1.1.1:53,8.8.8.8:53"
-          "--certificatesresolvers.cloudflare.acme.email=acme@shyim.de"
-          "--certificatesresolvers.cloudflare.acme.storage=/data/acme.json"
-
-          "--log.level=INFO"
-          "--accesslog=true"
-        ];
-      };
+      ports = [
+        "80:80"
+        "443:443"
+        "443:443/udp"
+      ];
     };
 
-    volumes.traefik_data = { };
+    volumes.traefik_data = {};
   };
 }
